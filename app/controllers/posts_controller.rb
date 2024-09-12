6fe230ddccbd871e4f906ea639cbfe46162fc13f
chie-hira <capella.a36@gmail.com>
2024-09-12 11:02:24 +0900

    index,create作成
    railsではデータベースはデフォルトでsqlite3が使われている
    config/database.ymlに記述されており、db:migrateを実行することでデータベースが作成される

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
     end
+
+    # newとcreateはセットで使う
+    def create
+        # フォームから送られてきたデータを受け取ってインスタンス変数に代入
+        @post = Post.new(post_params)
+        
+        # posts_pathはroutes.rbでresourcesを定義したときに自動的に生成される
+        # posts_pathはposts#indexを指す
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def post_params
+        # ストロングパラメーター
+        # フォームから送られてきたデータのうち、許可されたデータだけを受け取る
+        # この場合、titleとcontentだけを受け取る
+        params.require(:post).permit(:title, :content)
+    end
 end
